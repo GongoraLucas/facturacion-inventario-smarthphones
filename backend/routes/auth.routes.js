c520f9d7665ede66517cc39a7b5b64ca0c87c01b
GongoraLucas <gongoralucas59@gmail.com>
2025-07-02 00:29:02 -0500

    Proteccion de rutas en el backend

--- backend/routes/auth.routes.js
+++ backend/routes/auth.routes.js
@@ -1,10 +1,15 @@
-const {registerUser,loginUser} = require("../controllers/auth.controllers")
+const {registerUser,loginUser,getPerfil} = require("../controllers/auth.controllers")
+const {protect} =require("../middlewares/authMiddlewares")
 const express = require("express");
 
 const router = express.Router()
 
 router.post("/register",registerUser)
 router.post("/login",loginUser)
+
+router.get("/perfil",protect,getPerfil)
+
+
 
 
 module.exports = router
