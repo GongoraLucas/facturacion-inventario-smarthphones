d1bc381c5e9b28c8fb76643c3e360ce888445335
GongoraLucas <gongoralucas59@gmail.com>
2025-07-02 00:29:02 -0500

    Proteccion de rutas

--- backend/controllers/auth.controllers.js
+++ backend/controllers/auth.controllers.js
@@ -66,7 +66,12 @@ const loginUser = async (req,res)=>{
     }
 }
 
+const getPerfil = (req,res)=>{
+    res.status(200).json({user:req.user})
+
+}
 module.exports = {
     registerUser,
-    loginUser
+    loginUser,
+    getPerfil
 }
